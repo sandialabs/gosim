f165296c11a0b04e71e397fb0873cd4b95720c2f
John Floren <john@jfloren.net>
2012-03-21 16:16:46 -0700

    Updated for Go RC1, added license and README.

--- gosim.go
+++ gosim.go
@@ -8,31 +8,31 @@
 package main
 
 import (
-	"log"
 	"bufio"
+	"encoding/xml"
 	"fmt"
-	"os"
+	"github.com/floren/ellipsoid"
 	"io"
-	"xml"
-	"rand"
-	"time"
-	"net"
-	"strings"
 	"launchpad.net/mgo"
-	"launchpad.net/gobson/bson"
-	"github.com/npe9/ellipsoid"
+	"launchpad.net/mgo/bson"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-	handlers	map[string]func(*net.TCPConn, []string)()
-	people	map[string]*Person
-	session	*mgo.Session
-	osm		*Osm
+	handlers map[string]func(*net.TCPConn, []string)
+	people   map[string]*Person
+	session  *mgo.Session
+	osm      *Osm
 
 	randomizer *rand.Rand
-	geo ellipsoid.Ellipsoid
-	newPerson chan *Person
-	command chan Command
+	geo        ellipsoid.Ellipsoid
+	newPerson  chan *Person
+	command    chan Command
 )
 
 const (
@@ -42,23 +42,23 @@ const (
 )
 
 type Command struct {
-	Command	int
-	UID			string
+	Command int
+	UID     string
 }
 
 type Nd struct {
-	Ref int `xml:"attr"`
+	Ref int `xml:"ref,attr"`
 }
 
 type Tag struct {
-	K string `xml:"attr"`
-	V string `xml:"attr"`
+	K string `xml:"k,attr"`
+	V string `xml:"v,attr"`
 }
 
 type Way struct {
-	Nd   []Nd
+	Nd   []Nd	`xml:"nd"`	
+	Tag  []*Tag	`xml:"tag"`
 	Node []*Node
-	Tag  []*Tag
 }
 
 func (w *Way) RandNode() Node {
@@ -70,13 +70,12 @@ func (w *Way) Nodes() []*Node {
 }
 
 type Relation struct {
-
 }
 
 type Node struct {
-	Id  int     `xml:"attr"`
-	Lat float64 `xml:"attr"`
-	Lon float64 `xml:"attr"`
+	Id  int     `xml:"id,attr"`
+	Lat float64 `xml:"lat,attr"`
+	Lon float64 `xml:"lon,attr"`
 	Way []*Way
 }
 
@@ -100,13 +99,13 @@ func (n Node) Eq(m Node) bool {
 
 type Osm struct {
 	XMLName  xml.Name `xml:"osm"`
-	Node     []*Node
-	Way      []*Way
-	Relation []*Relation
+	Node     []*Node	`xml:"node"`
+	Way      []*Way		`xml:"way"`
+	Relation []*Relation	`xml:"relation"`
 }
 
-func NewOsmXml(rd io.Reader) (o *Osm, err os.Error) {
-	err = xml.Unmarshal(rd, &o)
+func NewOsmXml(rd io.Reader) (o *Osm, err error) {
+	err = xml.NewDecoder(rd).Decode(&o)
 	if err != nil {
 		return
 	}
@@ -137,11 +136,11 @@ func (o *Osm) RandWay(n *Node) *Way {
 
 // This is a representation of a single person. Probably sub-optimal.
 type Person struct {
-	UID	string		/* Which android device is this? Should be unique.*/
-	loc	Node
-	dest	Node
-	bearing	float64
-	way	*Way
+	UID     string /* Which android device is this? Should be unique.*/
+	loc     Node
+	dest    Node
+	bearing float64
+	way     *Way
 	enabled bool
 }
 
@@ -180,34 +179,35 @@ func (p *Person) SetDest(n Node) {
 	p.dest = n
 }
 
-
 // This is the representation of a Person that actually gets pushed to the database.
 // It's simple because we only really care about Latitude and Longitude
 type DBPerson struct {
-	UID	string
-	Lat		float64
-	Lon		float64
+	UID string
+	Lat float64
+	Lon float64
 }
 
 func UpdatePeople() {
-	if (session != nil) {
+	if session != nil {
 		c := session.DB("megadroid").C("phones")
 		for _, p := range people {
 			fmt.Printf("updating %v\n", p)
-			_, err := c.Upsert(bson.M{"uid": p.UID}, &DBPerson{p.UID, p.Loc().Lat, p.Loc().Lon}) 
-			if err != nil {panic(err)}
+			_, err := c.Upsert(bson.M{"uid": p.UID}, &DBPerson{p.UID, p.Loc().Lat, p.Loc().Lon})
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 }
 
 func init() {
-	handlers = map[string]func(*net.TCPConn, []string)(){ "Tpos":HandlePos,
-											"Tstart":HandleStart,
-											"Tstop":HandleStop,
-											"Tpause":HandlePause,
-											"Tcontinue":HandleCont }
+	handlers = map[string]func(*net.TCPConn, []string){"Tpos": HandlePos,
+		"Tstart":    HandleStart,
+		"Tstop":     HandleStop,
+		"Tpause":    HandlePause,
+		"Tcontinue": HandleCont}
 
-	randomizer = rand.New(rand.NewSource(time.Nanoseconds()))
+	randomizer = rand.New(rand.NewSource(time.Now().UnixNano()))
 	geo = ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.Longitude_is_symmetric, ellipsoid.Bearing_is_symmetric)
 
 	newPerson = make(chan *Person)
@@ -229,7 +229,7 @@ func init() {
 				case CONTINUE:
 					people[c.UID].enabled = true
 				case STOP:
-					people[c.UID] = nil, false
+					delete(people, c.UID)
 				}
 			case <-ticker.C:
 				for _, p := range people {
@@ -245,7 +245,7 @@ func init() {
 }
 
 func main() {
-	var err os.Error
+	var err error
 	f, err := os.Open(os.Args[1])
 	defer f.Close()
 	if err != nil {
@@ -254,17 +254,21 @@ func main() {
 
 	osm, err = NewOsmXml(f)
 
-	session, err = mgo.Mongo("localhost")
-	if err != nil { panic(err) } 
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
 	defer session.Close()
 	c := session.DB("megadroid").C("phones")
 	c.DropCollection()
 
-	rand.Seed(time.Nanoseconds() % 1e9)
+	rand.Seed(time.Now().UnixNano() % 1e9)
 
 	addr, _ := net.ResolveTCPAddr("tcp", ":4001")
 	l, err := net.ListenTCP("tcp", addr)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
 	for {
 		c, _ := l.AcceptTCP()
@@ -275,20 +279,20 @@ func main() {
 
 // Functions after this point are used to handle the network protocol.
 
-func readline(b *bufio.Reader) (p []byte, err os.Error) {
+func readline(b *bufio.Reader) (p []byte, err error) {
 	if p, err = b.ReadSlice('\n'); err != nil {
-		return nil, err;
+		return nil, err
 	}
-	var i int;
+	var i int
 	for i = len(p); i > 0; i-- {
 		if c := p[i-1]; c != '\r' && c != '\n' {
-			break;
+			break
 		}
 	}
-	return p[0:i], nil;
+	return p[0:i], nil
 }
 
-func HandleStart(c *net.TCPConn, args []string) () {
+func HandleStart(c *net.TCPConn, args []string) {
 	uid := args[0]
 
 	fmt.Printf("Creating new wanderer with ID = %v\n", uid)
@@ -306,7 +310,7 @@ func Rerror(c *net.TCPConn) {
 
 // We need to talk to the database, unfortunately, because if we scale this up to multiple machines,
 // it's quite likely that the UID you're asking about will be simulated on a different computer
-func HandlePos(c *net.TCPConn, args []string) () {
+func HandlePos(c *net.TCPConn, args []string) {
 	if args != nil {
 		db := session.DB("megadroid").C("phones")
 		fmt.Printf("arguments = %v\n", args)
@@ -319,30 +323,30 @@ func HandlePos(c *net.TCPConn, args []string) () {
 	}
 }
 
-func HandleStop(c *net.TCPConn, args []string) () {
+func HandleStop(c *net.TCPConn, args []string) {
 	if args != nil {
 		uid := args[0]
-		command<- Command{STOP, uid}
+		command <- Command{STOP, uid}
 		fmt.Fprintf(c, "Rstop %s\n", uid)
 	} else {
 		Rerror(c)
 	}
 }
 
-func HandlePause(c *net.TCPConn, args []string) () {
+func HandlePause(c *net.TCPConn, args []string) {
 	if args != nil {
 		uid := args[0]
-		command<- Command{PAUSE, uid}
+		command <- Command{PAUSE, uid}
 		fmt.Fprintf(c, "Rpause %s\n", args[0])
 	} else {
 		Rerror(c)
 	}
 }
 
-func HandleCont(c *net.TCPConn, args []string) () {
+func HandleCont(c *net.TCPConn, args []string) {
 	if args != nil {
 		uid := args[0]
-		command<- Command{CONTINUE, uid}
+		command <- Command{CONTINUE, uid}
 		fmt.Fprintf(c, "Rcontinue %s\n", args[0])
 	} else {
 		Rerror(c)
@@ -355,7 +359,10 @@ func Handler(c *net.TCPConn) {
 
 	for {
 		line, err := readline(br)
-		if err != nil { fmt.Print("exiting\n"); return }
+		if err != nil {
+			fmt.Print("exiting\n")
+			return
+		}
 
 		tok := strings.Split(string(line), " ")
 
